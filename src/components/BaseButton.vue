08bc7c08eeef6b652c4ddbb654ac4317c57b0d6f
Mulia Sujiastuti Wahyono <muliasujiastuti@gmail.com>
2023-05-29 13:47:58 +0700

    add components and assets

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,40 @@
+<template>
+    <button @click="$emit('btn-click')">{{ txt }}</button>
+</template>
+
+<script>
+export default {
+    name: "BaseButton",
+    props: {
+        txt: String,
+    },
+};
+</script>
+
+<style lang="scss" scope>
+@use "sass:math";
+@function rem($pixels, $context: 16) {
+    @return (math.div($pixels, $context)) * 1rem;
+}
+:root {
+    --baskerville: "Libre Baskerville", serif;
+    font-display: swap;
+}
+
+button {
+    background: transparent;
+    border: 0;
+    font-family: var(--baskerville);
+    font-size: rem(12);
+    line-height: rem(2.5);
+    font-weight: 700;
+    color: #7d7d7d;
+    text-transform: uppercase;
+    cursor: pointer;
+    letter-spacing: rem(2);
+
+    &:hover {
+        color: #000;
+    }
+}
+</style>
---
+++ src/components/BaseHeader.vue
@@ -0,0 +1,80 @@
+<template>
+    <header>
+        <router-link :to="`/`" @click="$emit('reset-start-show')">
+            <img alt="Vue logo" src="../assets/shared/logo.svg"/>
+        </router-link>
+        <BaseButton
+            @btn-click="$emit('start-slide-show')"
+            :txt="startShow ? 'Stop Slideshow' : 'Start Slideshow'"
+        />
+    </header>
+</template>
+
+<script>
+import BaseButton from './BaseButton.vue';
+
+export default {
+    name: "BaseHeader",
+    components: {
+        BaseButton,
+    },
+    props: {
+        startShow: Boolean,
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+@use "sass:math";
+@function rem($pixels, $context: 16) {
+    @return (math.div($pixels, $context)) * 1rem;
+}
+
+header {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: space-between;
+    padding: rem(24);
+    position: relative;
+    margin-bottom: rem(24);
+
+    &:after {
+        content: "";
+        display: flex;
+        height: 1px;
+        background-color: #e5e5e5;
+        width: 100%;
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        right: 0;
+    }
+
+    button {
+        font-size: rem(9);
+
+    }
+    > a {
+        max-width: rem(117);
+    }
+}
+@media (min-width: rem(768)) {
+    header {
+        padding: rem(24) rem(40);
+        margin-block-end: rem(40);
+        button {
+            font-size: rem(12);
+        }
+    }
+}
+@media (min-width: rem(1440)) {
+    header {
+        padding-left: 0;
+        padding-right: 0;
+        > a {
+            max-width: rem(170);
+        }
+    }
+}
+</style>
